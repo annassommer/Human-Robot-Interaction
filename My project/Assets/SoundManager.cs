b81ac50804d01e0a1d3fb48e05415780db05782e
FaKock <49271000+FaKock@users.noreply.github.com>
2022-01-15 14:05:38 +0100

    Adding a SoundManager

---
+++ My project/Assets/SoundManager.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class SoundManager
+{
+    public enum Sound
+    {
+        OroVoice,
+        KyleVoice,
+        Background,
+        WateringPlant,
+        FootSteps
+
+    }
+
+    private static GameObject oneShotGameObject;
+    private static AudioSource oneShotAudioSource;
+
+    private static Dictionary<Sound, float> soundTimerDictionary;
+
+    public SoundAudioClip[] soundAudioClipArray;
+
+    [System.Serializable]
+    public class SoundAudioClip
+    {
+        public SoundManager.Sound sound;
+        public AudioClip audioClip;
+
+    }
+
+    private AudioClip GetAudioClip(Sound sound)
+    {
+        foreach (SoundAudioClip soundAudioClip in soundAudioClipArray)
+        {
+            if (soundAudioClip.sound == sound)
+            {
+                return soundAudioClip.audioClip;
+            }
+        }
+        Debug.LogError("Sound" + sound + "not found!!!");
+        return null;
+    }
+    
+    public void PlaySound(Sound sound) {
+        if (CanPlaySound(sound))
+        {
+            if (oneShotGameObject == null)
+            {
+                oneShotGameObject = new GameObject("One Shot Sound");
+                oneShotAudioSource = oneShotGameObject.AddComponent<AudioSource>();
+            }
+            oneShotAudioSource.PlayOneShot(GetAudioClip(sound));  
+        }
+
+    }
+    public void PlaySound(Sound sound, Vector3 position) {
+        if (CanPlaySound(sound))
+        {
+            GameObject soundGameObject = new GameObject("Sound");
+            soundGameObject.transform.position = position;
+            AudioSource audioSource = soundGameObject.AddComponent<AudioSource>();
+            audioSource.clip = GetAudioClip(sound);
+            audioSource.Play();
+            
+            Object.Destroy(soundGameObject, audioSource.clip.length);
+        }
+
+    }
+
+
+    private static bool CanPlaySound(Sound sound)
+    {
+        switch(sound)
+        {
+            default:
+                return true;
+            case Sound.FootSteps:
+                if (soundTimerDictionary.ContainsKey(sound))
+                {
+                    float lastTimePlayed = soundTimerDictionary[sound];
+                    float movemtTimeMax = 0.05f;
+                    if (lastTimePlayed + movemtTimeMax < Time.time)
+                    {
+                        soundTimerDictionary[sound] = Time.time;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return true;
+                }
+        }
+    }
+}
